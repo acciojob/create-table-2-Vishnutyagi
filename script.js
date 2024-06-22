bb264e744d93b5e4ce347f65ce00d58b2b9e4f6b
Vishnu tyagi <106895072+Vishnutyagi@users.noreply.github.com>
2024-06-22 14:28:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function createTable() {
-    //Write your code here
-  
+    var rn=prompt("Input number of rows");
+	var cn=prompt("Input number of columns")
+	let table=document.getElementById('myTable');
+	table.innerHTML = '';
+	for(let i=0; i<rn; i++){
+		var row=table.insertRow();
+		for(let j=0; j<cn; j++){
+			var cell=row.insertCell(j);
+			cell.innerHTML=(`Row-${i} Column-${j}`);
+		}
+	}
 }
